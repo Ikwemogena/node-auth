fa5f3123c0b7936dd93151f0921636cff50e511e
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-04-10 15:41:44 +0100

    Add router for handling routes

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -27,3 +28,5 @@ const MONGO_URL = 'mongodb+srv://pulley06arc:YACwAEGTAHeyhtCM@backenddb.oaeyx5c.
 mongoose.Promise = Promise
 mongoose.connect(MONGO_URL)
 mongoose.connection.on('error', (err: Error) => console.error(err))
+
+app.use('/', router())
