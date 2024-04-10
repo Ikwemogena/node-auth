4eb9647b567658aeeda41933c67383c106b4e86f
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-04-10 16:17:07 +0100

    Add isOwner middleware for checking if the current user is the owner of a resource

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -25,3 +25,23 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         res.sendStatus(400);
     }
 } 
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params
+        
+        const currentUserId = get(req, 'identity._id') as String;
+
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId !== id) {
+            return res.sendStatus(403);
+        }
+
+        next();
+    } catch (error) {
+        res.sendStatus(400);
+    }
+}
